ef601010747a7e35de93f105e0066271255b1e15
unknown <scindia.harsh@gmail.com>
2019-01-05 17:53:10 +0530

    done with modules

--- Let's get classy/class_module.py
+++ Let's get classy/class_module.py
@@ -10,3 +10,21 @@ rockets.append(rg.Rocket(2,6))
 rockets.append(rg.Rocket(3,7))
 rockets.append(rg.Rocket(5,9))
 rockets.append(rg.Rocket(8,15))
+
+# show where each rocket is
+for index, rocket in enumerate(rockets):
+    print("Rocket %d is at (%d, %d)." % (index, rocket.x, rocket.y))
+    plt.plot(rocket.x, rocket.y, 'rs', linewidth=2, linestyle='dashed', markersize=12)
+    
+    # move the 'rocket' one up
+    rocket.move_up()
+    print("New rocket position %d is at (%d, %d)." % (index, rocket.x, rocket.y))
+    # plot the new position
+    plt.plot(rocket.x, rocket.y, 'bs', linewidth=2, linestyle='dashed', markersize=12)
+    # move the rocket left
+    plt.plot(rocket.x, rocket.y, 'ys', linewidth=2, linestyle='dashed', markersize=12)
+
+# show graph legend to match colors with position
+plt.title("Positions from using module")
+plt.gca().legend(('original position', '^ - Moved up', '< - Moved left'))
+plt.show()
