ca10e0e5496aca7c19b46e83c8393b8b46dda4b1
unknown <scindia.harsh@gmail.com>
2019-01-05 17:41:48 +0530

    adding rockets

--- Let's get classy/class_module.py
+++ Let's get classy/class_module.py
@@ -2,3 +2,11 @@ import matplotlib.py as plt
 import rocket_module as rg
 
 # making rockets
+rockets=[]
+rockets.append(rg.Rocket())
+rockets.append(rg.Rocket(0,2))
+rockets.append(rg.Rocket(1,4))
+rockets.append(rg.Rocket(2,6))
+rockets.append(rg.Rocket(3,7))
+rockets.append(rg.Rocket(5,9))
+rockets.append(rg.Rocket(8,15))
