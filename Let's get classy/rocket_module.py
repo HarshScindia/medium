3be61478f53b684502fd9174c3ea7d6be3e77372
unknown <scindia.harsh@gmail.com>
2019-01-05 17:33:36 +0530

    member functions

--- Let's get classy/rocket_module.py
+++ Let's get classy/rocket_module.py
@@ -6,3 +6,17 @@ class Rocket():
     def __init__(self, x=0, y=0):
         # each rocket has (x,y) position; user or calling function has choice
         # of passing in x and y value, or by default they are set at 0
+        self.x = x
+        self.y = y
+
+        def move_up(self):
+            self.y += 1
+
+        def move_down(self):
+            self.y -= 1
+
+        def move_right(self):
+            self.x += 1
+
+        def move_left(self):
+            self.x -= 1
