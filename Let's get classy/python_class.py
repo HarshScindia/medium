d35b16b55ffcaa3345465bd98eeb0ccbc19b127d
unknown <scindia.harsh@gmail.com>
2019-01-05 16:44:11 +0530

    added rockets

--- Let's get classy/python_class.py
+++ Let's get classy/python_class.py
@@ -20,4 +20,13 @@ class Rocket():
     def move_left(self):
         self.x -= 1
 
+# Make a series of rockets - x,y postions
+rockets = []
+rockets.append(Rocket())
+rockets.append(Rocket(0, 2))
+rockets.append(Rocket(1, 4))
+rockets.append(Rocket(2, 6))
+rockets.append(Rocket(3, 7))
+rockets.append(Rocket(5, 9))
+rockets.append(Rocket(8, 15))
 
