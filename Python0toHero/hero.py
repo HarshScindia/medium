f0815a7f2642a39e27938b9fe2de874edd6ead18
unknown <scindia.harsh@gmail.com>
2019-01-08 23:28:55 +0530

    Conditionals

--- Python0toHero/hero.py
+++ Python0toHero/hero.py
@@ -1,1 +1,28 @@
 # source code from medium article python zero to hero
+# The basics
+## 1. Variables
+one = 1
+two = 2
+true_boolean = True
+false_boolean = False
+
+# string
+my_name = "Harshvardhan Scindia"
+
+
+# float
+book_price = 15.80
+
+# control flow : conditional statements
+
+if True:
+    print("Hello python if")
+
+if 2 > 1:
+    print("2 is greater than 1")
+
+elif 1 > 2:
+    print("1 is greater than 2")
+
+else:
+    print("1 is equal to 2")
